dbdc2e860f79c6e525ac28bbe509c559a2d5c563
Fernandez Francisco <Franfer0301@gmail.com>
2024-10-27 20:21:22 -0300

    Update main.py

--- master/main.py
+++ master/main.py
@@ -11,5 +11,11 @@ lora = LoRa(spi, cs_pin=5, reset_pin=4, dio0_pin=2)
 node = DSRNode("Pancho",lora)
 
 while True:
+    """
     node.send_hello()
+    time.sleep(5)
+    """
+    node.broadcast_rreq("Morty")
+    time.sleep(5)
+    node.receive_message()
     time.sleep(5)
