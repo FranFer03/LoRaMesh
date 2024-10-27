73442d5309ad379db7d4cd8e4d85de3eea7dcc54
Fernandez Francisco <Franfer0301@gmail.com>
2024-10-27 19:47:59 -0300

    Update main.py

--- master/main.py
+++ master/main.py
@@ -1,16 +1,15 @@
 import time
 from machine import SPI, Pin
 from LoRa import LoRa
+from DSRNode import DSRNode
 import json
+
 
 spi = SPI(1, baudrate=3000000, polarity=0, phase=0, sck=Pin(18), mosi=Pin(23), miso=Pin(19)) 
 lora = LoRa(spi, cs_pin=5, reset_pin=4, dio0_pin=2)
 
-data = "RREP:A:C:123:B"
-
-data_str = json.dumps(data)
+node = DSRNode("Pancho",lora)
 
 while True:
-    print(data_str)
-    lora.send(data_str)
+    node.send_hello()
     time.sleep(5)
