b28eb1885d0b45590492df71b4e19e8bce0e364a
Nahuel Ontivero <132394148+nahuelontivero6@users.noreply.github.com>
2024-11-11 21:05:10 -0300

    Test

--- pico-pi4/pico_micropython/usb_serial.py
+++ pico-pi4/pico_micropython/usb_serial.py
@@ -1,26 +1,34 @@
 import sys
 import time
+import ujson
+
+# Definir el diccionario de datos
+query = {'RREP': [], 'RREQ': [['784317687', 'A', 'B'], ['784317689', 'A', 'B']], 'DATA': [], 'RESP': []}
 
 def main():
     while True:
         try:
             comando = sys.stdin.readline().strip()  # Intentar leer un comando
             if comando:
-                #print(f"Comando recibido: {comando}")   # Imprimir en sys.stdout (se envía por el puerto USB)
-            
-                if comando == 'REDES':
+                if comando == 'RUTAS':
                     print('Código prueba REDES')
                 elif comando == 'CAMINOS':
                     print('Código prueba CAMINOS')
                 elif comando == 'PAQUETE':
                     print('Código prueba PAQUETE')
-                elif comando == 'SINCRONIZAR':
+                elif comando == 'DATOS':
                     print('Código prueba SINCRONIZAR')
+                elif comando == 'ENVIAR_QUERY':
+                    # Convertir `query` a JSON y enviarlo por USB
+                    query_json = ujson.dumps(query)
+                    print(query_json)  # Esto envía los daZtos al puerto USB
+                    #print("Datos enviados a través del puerto USB")
+
                 else:
                     print('Comando no reconocido')
 
         except Exception as e:
-            pass  # No hacer nada si no hay datos
+            print("Error:", e)
 
         time.sleep(0.1)
 
